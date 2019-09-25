c1db8bb2f442983d72c71070554f05cd843cced0
Shannon Gamby <shannongamby@hotmail.com>
2019-09-25 20:02:42 +0100

    clean up pricing test

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -9,11 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -30,13 +27,11 @@ public class PricingServiceApplicationTests {
 
 	@Test
 	public void testGetPrice() throws Exception{
-		ArrayList<String> content = new ArrayList<>();
 		mvc.perform(get("/services/price?vehicleId=1"))
 				.andExpect(status().isOk())
-				.andDo(mvcResult -> content.add(mvcResult.getResponse().getContentAsString()));
-		assertThat(content.get(0), containsString("price"));
-		assertThat(content.get(0), containsString("currency"));
-		assertThat(content.get(0), containsString("vehicleId"));
+				.andExpect(jsonPath("$.price").exists())
+				.andExpect(jsonPath("$.currency").value("USD"))
+				.andExpect(jsonPath("$.vehicleId").value(1));
 	}
 
 }
