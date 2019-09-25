32237e908b911012d3e45df0e13fe78ec5d8b1bb
Shannon Gamby <shannongamby@hotmail.com>
2019-09-25 14:02:36 +0100

    add test for pricing-service

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,42 @@
+
 package com.udacity.pricing;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetPrice() throws Exception{
+		ArrayList<String> content = new ArrayList<>();
+		mvc.perform(get("/services/price?vehicleId=1"))
+				.andExpect(status().isOk())
+				.andDo(mvcResult -> content.add(mvcResult.getResponse().getContentAsString()));
+		assertThat(content.get(0), containsString("price"));
+		assertThat(content.get(0), containsString("currency"));
+		assertThat(content.get(0), containsString("vehicleId"));
 	}
 
 }
